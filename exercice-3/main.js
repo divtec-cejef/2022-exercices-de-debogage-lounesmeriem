fb41b051808c4a54585ccbe8a736de83a9b3ac96
lounmer <meriem.lounes@divtec.ch>
2023-09-12 12:03:43 +0200

    corriger exercice 3

--- exercice-3/main.js
+++ exercice-3/main.js
@@ -1,9 +1,9 @@
-(function main() {
-	let lancementOk = false;
-	let statutEquipage  = true;
-	let StatutOrdinateur = 'vert';
 
-	if (statutEquipage  &&& StatutOrdinateur === 'vert') {
+
+	let statutEquipage  = true;
+	let statutOrdinateur = 'vert';
+
+	if (statutEquipage  && statutOrdinateur === 'vert') {
 		console.log('Equipage et ordinateur approuvés.');
 		lancementOk = true;
 	} else {
@@ -12,10 +12,10 @@
 	}
 
 	if (lancementOk) {
-		console.log(("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
+		console.log("10, 9, 8, 7, 6, 5, 4, 3, 2, 1...");
 		console.log("Allumage...");
 		console.log("Décollage!");
 	} else {
 		console.log("Lancement annulé.");
 	}
-}()); // Main IIFE
+
